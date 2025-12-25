21000dab36673fd01cd4d8c3bcf27ea63cb4820a
Dharmaraj T <t.rajdharma9595@gmail.com>
2025-12-25 13:38:47 +0530

    AboutPage added

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import FormOne from "./Components/FormOne";
 import FormTwo from "./Components/FormTwo";
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomePage from "./Components/HomePage";
+import About from "./Components/AboutPage";
 
 
 
@@ -42,7 +43,7 @@ function App() {
 
   {/* <ChangeName/>  */}
 
-       <ApiCall/> 
+       {/* <ApiCall/>  */}
 
        {/* <CardComp/>   */}
 
@@ -57,6 +58,7 @@ function App() {
       {/* <FormTwo/> */}
 
       <HomePage/>
+      <About/>
 
     </div>
   )
--- src/Components/AboutPage.jsx
+++ src/Components/AboutPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../Components/";
+import "../Components/AboutPage.css";
 
 function About() {
   return (
